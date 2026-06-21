41cbc167fcd374a1549e247a6daaf894c08aff31
Your Name <kratikaverma68@gmail.com>
2026-06-21 11:09:16 +0530

    Fix Render deployment

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const fs = require("fs");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
@@ -62,7 +62,9 @@ app.post("/api/login", (req, res) => {
 
   res.status(200).json({ message: "Login successful!", student });
 });
-
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 // ✅ Start the server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
